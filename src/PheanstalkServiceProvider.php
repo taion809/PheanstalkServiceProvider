42d2be8d9fb7ccf9e6b75e3bea520e5bc8430497
Greg ORIOL <greg@gregoriol.net>
2017-05-08 16:08:01 +0200

    Updated for Silex 2 (#2)
    * Update for Silex 2
    
    * Update for Silex 2
    
    * Fixed missing import
    
    * Updated README with new Silex 2 syntax
    
    * Cleanup of unused code

--- src/PheanstalkServiceProvider.php
+++ src/PheanstalkServiceProvider.php
@@ -1,25 +1,21 @@
 <?php
 namespace PheanstalkServiceProvider;
 
-use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 use Pheanstalk\Pheanstalk;
 use Pheanstalk\PheanstalkInterface;
 
 class PheanstalkServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
-        $app['pheanstalk'] = $app->share(function () use ($app) {
+        $app['pheanstalk'] = function () use ($app) {
             $host = $app['pheanstalk.hostname'] ? $app['pheanstalk.hostname'] : 'localhost';
             $port = $app['pheanstalk.port'] ? $app['pheanstalk.port'] : PheanstalkInterface::DEFAULT_PORT;
             $timeout = $app['pheanstalk.timeout'] ? $app['pheanstalk.timeout'] : null;
 
             return new Pheanstalk($host, $port, $timeout);
-        });
-    }
-
-    public function boot(Application $app)
-    {
+        };
     }
 }
